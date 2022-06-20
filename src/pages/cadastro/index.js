f10043ba55fec0ffa4d62fdc74220c2f2d79aa49
RicardoJun10r <ricardojunior0110@gmail.com>
2022-06-20 15:42:42 -0300

    Adicionando a página cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,31 +1,56 @@
 import React, { useState } from "react";
 import './style.css';
+import { api } from "../../services/api";
 import { Container } from "reactstrap";
 import { useNavigate } from "react-router-dom";
-import { api, setUsers } from "../../services/api";
+import NavbarUser from "../../components/NavbarUser";
+
+const initialValues = {
+    nome: '',
+    email: '',
+    senha: '',
+}
 
 function Cadastro(){
 
-    const [nome, setNome] = useState("");
-    const [email, setEmail] = useState("");
-    const [matricula, setMatricula] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
+    const [values, setValues] = useState(initialValues);
     const navigate = useNavigate();
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log("foi")
-        setUsers(nome, email, matricula, password);
+    function onChange(e){
+        const { name, value } = e.target;
+
+        setValues({
+            ...values, [name]: value
+        });
     }
-    
+
+    function cadastrar(e) {
+        e.preventDefault();
+
+        const userData = {
+            nome: values.nome,
+            email: values.email,
+            senha: values.senha,
+        }
+        api.post("https://apiufersaflix.herokuapp.com/usuario/", userData)
+        .then((response) => {
+            console.log("response: " + response.status);
+            console.log("data: " + response.data);
+            navigate('/login')
+        }).catch((err)=>{
+            console.error("erro cadastrar: " + err);
+        })
+    }
+
     const getBack = (e) => {
         e.preventDefault();
         navigate('/login')
     }
+
     return(
-        <div style={{ backgroundColor: "#393939", color: "white", position: "absolute", height: "100%", width: "100%" }}>
+        <div className="container">
             <Container>
+                <NavbarUser/>
                 <form className="formulario">
                     <div>
                         <h1 className="titulo">CADASTRE-SE</h1>
@@ -34,57 +59,43 @@ function Cadastro(){
                     <br/>
                     <input 
                     type='text'
-                    value={nome}
                     id='nome'
                     name="nome"
-                    onChange={(e)=>{setNome(e.target.value)}}
+                    value={values.nome}
+                    onChange={onChange}
                     />
                     <br/>
                     <label>E-MAIL</label>
                     <br/>
                     <input 
                     type='email'
-                    value={email}
                     id='email'
                     name="email"
-                    onChange={(e)=>{setEmail(e.target.value)}}
-                    />
-                    <br/>
-                    <label>MATRÍCULA</label>
-                    <br/>
-                    <input 
-                    type='text'
-                    value={matricula}
-                    id='matricula'
-                    name="matricula"
-                    onChange={(e)=>{setMatricula(e.target.value)}}
+                    value={values.email}
+                    onChange={onChange}
                     />
                     <br/>
                     <label>SENHA</label>
                     <br/>
                     <input 
                     type='password'
-                    value={password}
                     id='password'
-                    name='password'
-                    onChange={(e)=>{setPassword(e.target.value)}}
+                    name='senha'
+                    value={values.senha}
+                    onChange={onChange}
                     />
                     <br/>
-                    <label>CONFIRMAR SENHA</label>
-                    <br/>
-                    <input 
-                    type='password'
-                    value={confirmPassword}
-                    name='confirmPassword'
-                    id="confirmPassword"
-                    onChange={(e)=>{setConfirmPassword(e.target.value)}}
-                    />
                     <div>
-                        <button className="botao-submit" type="submit" onClick={onSubmit}>CADASTRAR</button>
+                        <button className="botao-submit" type="submit" onClick={cadastrar}>CADASTRAR</button>
                         <button className="botao-voltar" type="submit" onClick={getBack}>VOLTAR</button>
                     </div>
                 </form>
             </Container>
+            <p>{values.nome}</p>
+            <br/>
+            <p>{values.email}</p>
+            <br/>
+            <p>{values.senha}</p>
         </div>
     )
 }
