c4d4e3abadb5721d0647cdfe36aa16bea53409a8
RicardoJun10r <ricardojunior0110@gmail.com>
2022-06-20 21:49:57 -0300

    adicionando api do tmdb

--- src/services/api.js
+++ src/services/api.js
@@ -15,3 +15,52 @@ export const getUsers = async() => {
 export const setUsers = async (nome, email, matricula, password) => {
     return api.post("/usuario", {nome, email, matricula, password})
 }
+
+
+// api do TMDB
+
+const API_KEY = "de98b03affef6fe0471936aa8ee6afc0";
+
+const categories = [
+    {
+        name: "trending",
+        title: "Em alta",
+        path: `/trending/all/day?api_key=${API_KEY}`,
+    },
+    {
+        name: "netflixOriginals",
+        title: "Originais Netflix",
+        path: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    },
+    {
+        name: "topRated",
+        title: "Populares",
+        path: `/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
+    },
+    {
+        name: "comedy",
+        title: "Comédias",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
+    },
+    {
+        name: "romances",
+        title: "Romances",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
+    },
+    {
+        name: "documentaries",
+        title: "Documentários",
+        path: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+    },
+];
+
+export const getMovies = async (path) => {
+    try {
+        let url = `https://api.themoviedb.org/3${path}`;
+        const response = await fetch(url);
+    } catch (error) {
+        console.log("Error getMovies: " + error);       
+    }
+}
+
+export default categories;
